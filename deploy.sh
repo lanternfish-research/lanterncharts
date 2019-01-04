058c5186143c7ba21d2aafd99188bac8aeaffd88
wsq <mywsq961@gmail.com>
2019-01-04 15:41:43 +0800

    update deploy bash

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,8 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git config user.name "CircleCI"
+git config user.email "circleci@lanternfish.cn"
 git push -f https://github.com/lanternfish-research/lanterncharts.git master:gh-pages
 
 cd -
