8658368e342e463cc53e82b399b456d5234b0763
wsq <mywsq961@gmail.com>
2019-01-04 15:57:55 +0800

    fingerprint

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git config user.email "circleci@lanternfish.cn"
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/lanternfish-research/lanterncharts.git master:gh-pages
+git push -f git@github.com:lanternfish-research/lanterncharts.git master:gh-pages
 
 cd -
